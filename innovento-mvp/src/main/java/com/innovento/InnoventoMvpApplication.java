903d60e9050686a9a42e924d498ffa116b4d09d7
Izhan Khan <izhankhan001@gmail.com>
2023-01-18 00:35:30 +0530

    18-01-2023 commit

--- innovento-mvp/src/main/java/com/innovento/InnoventoMvpApplication.java
+++ innovento-mvp/src/main/java/com/innovento/InnoventoMvpApplication.java
@@ -2,14 +2,20 @@ package com.innovento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-public class InnoventoMvpApplication {
+public class InnoventoMvpApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(InnoventoMvpApplication.class, args);
 	}
-
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(InnoventoMvpApplication.class);
+    }
 }
